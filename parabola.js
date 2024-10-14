5c2c320b9b111e5c3486e98b335c56579937d7b2
Tanakai <tanakatanakakai@gmail.com>
2024-10-15 03:02:15 +0900

    Refactor

--- parabola.js
+++ parabola.js
@@ -1,7 +1,16 @@
 /*
 Draws a parabola on the canvas.
-@param {number} a - explanation.
+@param {CanvasRenderingContext2D} ctx - rendering context for the canvas
+@param {number} x - The x-axis (horizontal) coordinate of the parabola's focus.
+@param {number} y - The y-axis (vertical) coordinate of the parabola's focus.
+@param {number} a - The focal length of the parabola, where the focus is located at (0, a) and the directrix at y = -a before rotation. 
+@param {number} rotation - The rotation of the parabola around the focus, expressed in radians.
+@param {number} startPos - The starting x-coordinate on the x-axis for drawing the portion of the parabola.
+@param {number} endPos - The ending x-coordinate on the x-axis for drawing the portion of the parabola.
 */
-function drawParabola(a) {
-    
+function drawParabola(ctx, x, y, a, rotation, startPos, endPos) {
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    ctx.quadraticCurveTo(cpX, cpY, endX, endY);
+    ctx.stroke();
 }
